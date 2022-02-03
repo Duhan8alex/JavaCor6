013f823abb97da5b08f9b21f697360c012844364
Alexandr <95639393+Duhan8alex@users.noreply.github.com>
2022-02-03 20:30:44 +0700

    Add files via upload

---
+++ HomeWorkSix.java
@@ -0,0 +1,44 @@
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import java.io.IOException;
+
+public class HomeWorkSix {
+    private static final String BASE_HOST = "dataservice.accuweather.com";
+    private static final String FORECAST = "forecasts";
+    private static final String API_VERSION = "v1";
+    private static final String FORECAST_TYPE = "daily";
+    private static final String FORECAST_PERIOD = "5day";
+
+    private static final String NOVOSIBIRSK_KEY = "27707_PC";
+    private static final String API_KEY = "uqhnAeaY0m3AQSGXv0ighpH6VY0MsDAA";
+
+    public static void main(String[] args) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+            HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host(BASE_HOST)
+                .addPathSegment(FORECAST)
+                .addPathSegment(API_VERSION)
+                .addPathSegment(FORECAST_TYPE)
+                .addPathSegment(FORECAST_PERIOD)
+                .addPathSegment(NOVOSIBIRSK_KEY)
+                .addQueryParameter("apikey", API_KEY)
+                .addQueryParameter("language", "ru-ru")
+                .addQueryParameter("metric", "true")
+                .build();
+
+        System.out.println(url.toString());
+
+        // При необходимости указать заголовки
+        Request requesthttp = new Request.Builder()
+                .addHeader("accept", "application/json")
+                .url(url)
+                .build();
+
+        String jsonResponse = client.newCall(requesthttp).execute().body().string();
+        System.out.println(jsonResponse);
+    }
+}
